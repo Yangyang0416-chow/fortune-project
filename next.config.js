4a52fefe0251e8648ed859354d34ca4e58223598
Joanna Chow <yangyanga0416@gmail.com>
2025-01-12 22:16:36 +0800

    fix: update css configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const nextConfig = {
   },
   // 开启压缩
   compress: true,
-  // 图片优化
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    minimumCacheTTL: 60,
-  },
   // 生产环境优化
   productionBrowserSourceMaps: false,
   swcMinify: true,
